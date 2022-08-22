5e99ccb1bc6ae863b8327bb9f7f4c0f9488163b0
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-22 22:55:01 +0100

    added new lines to the file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle in the list
+ * 
+ * @list: sample list to check
+ * Return: 0 for success and 1 for failure 
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *buffer;
+    
+    buffer = list;
+    for (buffer = buffer->next; buffer; buffer = buffer->next)
+    {
+        if (buffer == list) 
+            return (1);
+    }
+    
+    return (0);
+}
