cba8ef63dee8d98af8cf7b6604bcbbe701e7f612
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-24 09:04:10 +0100

    worked on task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+for i in range(100):
+    n = i
+    # reverse the integer number using the while loop
+    revs_number = 0
+    while n > 0:
+        # Logic
+        remainder = n % 10
+        revs_number = (revs_number * 10) + remainder
+        n = n // 10
+    if i == 89:
+        print("{}".format(i))
+        break
+    if revs_number >= i:
+        print("{:02d}".format(i), end=", ")
