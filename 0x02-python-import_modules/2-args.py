628607af7facaefdab529b31f6765495a2170152
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-25 19:31:57 +0100

    done with task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ from sys import argv
 
 def main():
     i = len(argv) - 1
-    print("{} {}.".format(i, 'argument' if i == 1 else 'arguments'))
+    print("{} {}{}".format(i,
+                           'argument' if i == 1 else 'arguments',
+                           '.' if i == 0 else ':'))
     if i != 0:
         for j in range(1, i+1):
             print("{}: {}".format(j, argv[j]))
