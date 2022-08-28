98c0c173284c0c9411e2cb97bd9255f0cf9b13d3
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-28 23:43:09 +0100

    fixed task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if (idx > len(my_list)) or (idx < 0):
-        return None
-    return (my_list[idx])
+    if 0 <= idx < len(my_list):
+        return (my_list[idx])
+    return None
