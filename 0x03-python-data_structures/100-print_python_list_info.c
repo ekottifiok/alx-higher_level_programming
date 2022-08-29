7d2e2224cfc00a9a1158f272a0d29286f1fa7332
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-29 22:13:27 +0100

    done with task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,18 @@
 #include "lists.h"
-#include <python3.4m/Python.h>
-#include <stdio.h>
-void print_python_list_info(PyObject *p __attribute__((unused)))
+
+/**
+ * print_python_list_info - Prints some info of a python list
+ *
+ * @p: python object that carries the list
+ */
+void print_python_list_info(PyObject *p)
 {
-    puts("Concept works");
-    
+	int obj_size, iter;
+	PyObject *object;
+
+	obj_size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", obj_size);
+	printf("[*] Alocated = %d\n", ((PyListObject *)p)->allocated);
+	for (iter = 0; iter < obj_size; iter++)
+		printf("Element %d: %s\n", iter, Py_TYPE(object)->tp_name);
 }
