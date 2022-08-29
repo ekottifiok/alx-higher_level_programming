969749cef94cac067cae3447fe3650554b94c997
Ifiok Ekott <ekottifiok@gmail.com>
2022-08-29 12:51:42 +0100

    modded task 7 to run only 2 times

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    for i in range(len(tuple_b)):
-        list_a[i] += list_b[i]
-    return (tuple(list_a))
+    answer = []
+    for i in range(2):
+        try:
+            j = list_a[i]
+        except IndexError:
+            j = 0
+        try:
+            k = list_b[i]
+        except IndexError:
+            k = 0
+        answer.append(j + k)
+    return (tuple(answer))
