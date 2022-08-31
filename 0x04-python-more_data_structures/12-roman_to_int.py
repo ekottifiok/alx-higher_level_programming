ee2873c4f2632dc3d3958154bc4bcc47205a0a85
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-01 00:28:04 +0100

    fixed task 12 pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,22 +17,24 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
-    for str in roman_string:
-        str_int = 0 if str not in roman.keys() else roman.get(str)
-        if prev == str_int:
-            # if chain == 0:
-            #     chain = prev
-            chain += str_int
-        elif prev != 0 and prev < str_int:
-            result -= prev + chain
-            result += str_int - prev
-            chain = 0
-        elif result < str_int and result != 0:
-            result = str_int - result
-        else:
-            result += str_int + chain
-            chain = 0
-        prev = str_int
-    if chain != 0:
-        result += chain
-    return result if result > 0 else 0
+    if isinstance(roman_string, string):
+        for str in roman_string:
+            str_int = 0 if str not in roman.keys() else roman.get(str)
+            if prev == str_int:
+                # if chain == 0:
+                #     chain = prev
+                chain += str_int
+            elif prev != 0 and prev < str_int:
+                result -= prev + chain
+                result += str_int - prev
+                chain = 0
+            elif result < str_int and result != 0:
+                result = str_int - result
+            else:
+                result += str_int + chain
+                chain = 0
+            prev = str_int
+        if chain != 0:
+            result += chain
+        return result if result > 0 else 0
+    return None
