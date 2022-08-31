45c40053914f420e34211ccd5f66f78d3f6fa19f
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-01 00:30:48 +0100

    working on task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
 
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) or len(a_dictionary) > 0:
         score = -99999999
         for value in a_dictionary.values():
             if value > score:
