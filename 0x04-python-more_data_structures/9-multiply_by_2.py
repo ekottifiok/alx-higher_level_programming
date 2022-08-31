2b5cee4ad767a30416ee0a96c4d934d528760077
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-01 00:43:15 +0100

    fixed task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,4 @@
 
 def multiply_by_2(a_dictionary):
     if isinstance(a_dictionary, dict):
-        for key in a_dictionary.keys():
-            a_dictionary[key] = a_dictionary.get(key) * 2
-    return a_dictionary
+        return {key: a_dictionary[key]*2 for key in a_dictionary.keys()}
