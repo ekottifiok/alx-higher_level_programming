0ac55296ae5e60884907eed85f5d148f985adeb5
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-01 00:39:15 +0100

    fixed task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,15 @@
 
 def best_score(a_dictionary):
 
-    if isinstance(a_dictionary, dict) or len(a_dictionary) > 0:
-        score = -99999999
-        for value in a_dictionary.values():
-            if value > score:
-                score = value
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    score = None
+    min_score = -9999999999999
+    for key in a_dictionary.keys():
+        value = a_dictionary.get(key)
+        if value > min_score:
+            min_score = value
+            score = key
 
-        return score
-    return None
+    return score
+    
