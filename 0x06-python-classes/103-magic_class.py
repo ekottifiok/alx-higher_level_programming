ec359a112a6eaeada4d9e638e6c419c2a6e2a324
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-14 09:50:07 +0100

    fixed pycodestyle error for 103-magic

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,15 +2,16 @@
 import math
 from typing import Type
 
+
 class MagicClass:
-    
+
     def __init__(self, radius):
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self._MagicClass__radius = radius
-    
+
     def area(self):
-        return self._MagicClass__radius **2 * math.pi
-    
+        return self._MagicClass__radius ** 2 * math.pi
+
     def circumference(self):
         return 2 * math.pi * self._MagicClass__radius
