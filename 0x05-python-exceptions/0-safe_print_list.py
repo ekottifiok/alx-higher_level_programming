49b6a7547c7d192744bd3af811fb36d5349f1861
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-15 01:35:14 +0100

    added a typerror check to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[j]), end="")
             i += 1
-        except (IndexError,):
+        except (IndexError, TypeError):
             break
 
     print()
