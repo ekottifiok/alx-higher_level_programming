b7838659b70da7e1ea13bf5d2e62480ac728c47b
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-19 19:23:07 +0100

    made a typo in 1-rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self._width = value
 
@@ -31,6 +31,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self._width = value
