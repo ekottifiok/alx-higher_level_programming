9325ca1430bba48279271b784403d6fdf1666cc4
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-19 19:24:03 +0100

    corrected another bug in 1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,4 +33,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self._width = value
+        self._height = value
