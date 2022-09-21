12d961a4d3b580c4f3eb3a5107ca4e1009eaf154
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-21 09:23:43 +0100

    more scam of 0x07

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+# 0-add_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an integer addition function."""
+
 
 def add_integer(a, b=98):
-    """adds two numbers together
-
-    Args:
-        a (int): number 1
-        b (int, optional): number 2. Defaults to 98.
-
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError: a and b must be int or floats
-
-    Returns:
-        int: sum of the two numbers
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if type(a) not in [int, float]:
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    elif type(b) not in [int, float]:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    print(int(a) + int(b))
+    return (int(a) + int(b))
