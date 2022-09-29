8f268bb5ba069118234c72359987cd1e08277343
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-29 08:20:27 +0100

    added documentation to the pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""Pascal Triangle"""
+
+
 def pascal_triangle(n):
+    """handles the main idea behind the pascal triangle
+
+    Args:
+        n (int): size of the triangle
+
+    Returns:
+        array: the pascal triangle
+    """
+    if not isinstance(n, int):
+        return []
     list_re = []
     for i in range(n):
         value = [1]
@@ -8,7 +21,7 @@ def pascal_triangle(n):
         elif i == 1:
             list_re.append(value+[1])
         else:
-            for j in range(1,i):
+            for j in range(1, i):
                 value.append(list_re[-1][j] + list_re[-1][j-1])
             list_re.append(value+[1])
     return list_re
