ab8f7e69d898a21c0b97402584dcaf0305c09c10
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-29 08:23:07 +0100

    updated the logic in task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -18,10 +18,9 @@ def pascal_triangle(n):
         value = [1]
         if i == 0:
             list_re.append(value)
-        elif i == 1:
-            list_re.append(value+[1])
         else:
-            for j in range(1, i):
-                value.append(list_re[-1][j] + list_re[-1][j-1])
+            if i > 1:
+                for j in range(1, i):
+                    value.append(list_re[-1][j] + list_re[-1][j-1])
             list_re.append(value+[1])
     return list_re
