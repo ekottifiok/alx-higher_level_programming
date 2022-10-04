37ac85a2dd99c9477500aee721e626b1048eb973
Ifiok Ekott <ekottifiok@gmail.com>
2022-10-04 16:58:07 +0100

    added documentation to task 12

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""append after"""
 
 def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line
+    containing a specific string
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        search_string (str, optional): _description_. Defaults to "".
+        new_string (str, optional): _description_. Defaults to "".
+    """
     if filename == "" or search_string == "" or new_string == "":
         return
     with open(filename, 'r+') as file:
