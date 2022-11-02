084c19f9b220571791ad7105d99456e38bddbcdb
Ifiok Ekott <ekottifiok@gmail.com>
2022-11-02 13:56:44 +0100

    removed not null and added newline in line 2

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,6 @@
 -- Write a script that creates the table id_not_null on your MySQL server.
+
 CREATE TABLE IF NOT EXITST `id_not_null` (
     id INT DEFAULT 1,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) 
 );
