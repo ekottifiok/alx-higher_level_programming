ac4a27c5672f5391a79a5d8d18b3b4ae04fbed3d
Ifiok Ekott <ekottifiok@gmail.com>
2022-11-28 15:35:36 +0100

    done with task 4-7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
-console.log(arg ? `My number: ${arg}`: 'Not a number');
+console.log(arg ? `My number: ${arg}` : 'Not a number');
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+for (let index = 0; index < parseInt(process.argv[2]); index++) {
+    console.log('C is fun');
+}
