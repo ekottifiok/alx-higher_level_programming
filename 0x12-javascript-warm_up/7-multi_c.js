51b5122fcc16906b13e16041ce1c0a1c6d8363ba
Ifiok Ekott <ekottifiok@gmail.com>
2022-11-28 15:39:17 +0100

    added a check for when  no number was sent in task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (let index = 0; index < parseInt(process.argv[2]); index++) {
+if (typeof process.argv[2] === 'undefined') {
+  console.log('Missing number of occurrences');
+} else {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
     console.log('C is fun');
+  }
 }
