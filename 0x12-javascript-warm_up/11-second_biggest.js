e95b916f04281e8289e0e7677562984e0dbbe1e9
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-02 19:42:47 +0100

    updated task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,14 +6,24 @@ if (len < 4) {
 } else {
   let largest = parseInt(process.argv[2]);
   let secondLargest = parseInt(process.argv[3]);
-  for (let index = 2; index < len; index++) {
-    const x = parseInt(process.argv[index]);
-    if (x > largest) {
-      secondLargest = largest;
-      largest = x;
-    } else if (x > secondLargest) {
-      secondLargest = x;
+
+  if (len === 4)
+  {
+    if (largest > secondLargest){
+      console.log(secondLargest);
+    } else {
+      console.log(largest);
     }
+  } else {
+    for (let index = 2; index < len; index++) {
+      let x = parseInt(process.argv[index]);
+      if (x > largest) {
+        secondLargest = largest;
+        largest = x;
+      } else if (x > secondLargest) {
+        secondLargest = x;
+      }
+    }
+    console.log(secondLargest);
   }
-  console.log(secondLargest);
 }
