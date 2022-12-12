6357892f4275737eba2fa24c8b9e2cd49a1cf7a2
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-12 06:18:31 +0100

    updated task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,6 +3,9 @@
 // returns the reversed version of a list
 exports.esrever = function (list) {
   let listLen = list.length;
+  if (listLen === 0) {
+    return []
+  }
   const newArray = new Array(--listLen);
   list.forEach((element, index) => {
     newArray[listLen - index] = element;
