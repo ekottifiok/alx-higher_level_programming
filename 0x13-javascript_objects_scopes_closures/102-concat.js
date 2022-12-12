bc58f3a37a1e112a7d12a145b0053154805612c8
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-12 05:21:47 +0100

    fixed 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 let content = '';
 
-content.concat(fs.readFileSync(process.argv[2]));
-content.concat(fs.readFileSync(process.argv[3]));
+content = content.concat(fs.readFileSync(process.argv[2]));
+content = content.concat(fs.readFileSync(process.argv[3]));
 fs.writeFileSync(process.argv[4], content);
