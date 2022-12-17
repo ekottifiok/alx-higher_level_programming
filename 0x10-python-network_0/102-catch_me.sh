c915c5f19de61f11ae22fd0420c22f7ad86866a0
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-17 06:00:15 +0100

    changed the method to head

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that causes server to respond with message
-curl -sLX PUT -H "origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX HEAD -H "origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
