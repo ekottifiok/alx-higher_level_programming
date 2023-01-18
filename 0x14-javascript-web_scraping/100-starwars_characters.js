f0bd801d2aef9f09274cabda207cdfd9e8c053d6
KelvnPere <perekelvin66@gmail.com>
2023-01-19 00:34:26 +0100

    edited 100 and 101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,17 +4,11 @@ const request = require('request');
 
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, _, body) {
   if (error) { console.log(error); } else {
-    console.log(JSON.parse(body).characters.reduce((accumulator, element) => {
-        if (accumulator) {
-            accumulator += request('element', function (error, _, body) {
-                if (error) { console.log(error); } else {body.name}
-            }) + "\n"
-        } else {
-            accumulator = request('element', function (error, _, body) {
-                if (error) { console.log(error); } else {body.name}
-            })
+  JSON.parse(body).characters.forEach(element => {
+    request(element, function (error, _, body) {
+        if (error) {console.log(error);} else {
+            console.log(JSON.parse(body).name);
         }
-        return accumulator;
-    }, null));
-  }
+    })
+  });}
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+function handleRequest (array, iter) {
+  if (array.length === iter) return;
+  request(array[iter], function (error, _, body) {
+    if (error) { console.log(error); } else {
+      console.log(JSON.parse(body).name);
+    }
+    handleRequest(array, iter + 1);
+  });
+}
+
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, _, body) {
+  if (error) { console.log(error); } else { handleRequest(JSON.parse(body).characters, 0); }
+});
