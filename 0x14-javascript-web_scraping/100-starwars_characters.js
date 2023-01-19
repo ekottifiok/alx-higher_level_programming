ee47837e9f0e115a3f92faebae0fd216ad3c9d19
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-19 11:02:46 +0100

    fixed a semistandard errro

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,11 +4,12 @@ const request = require('request');
 
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, _, body) {
   if (error) { console.log(error); } else {
-  JSON.parse(body).characters.forEach(element => {
-    request(element, function (error, _, body) {
-        if (error) {console.log(error);} else {
-            console.log(JSON.parse(body).name);
+    JSON.parse(body).characters.forEach(element => {
+      request(element, function (error, _, body) {
+        if (error) { console.log(error); } else {
+          console.log(JSON.parse(body).name);
         }
-    })
-  });}
+      });
+    });
+  }
 });
